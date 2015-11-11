bcad5a780f52d2a4e2fd9e5fe04ee2191d1d63c3
tonypiwko <tony.piwko@outlook.com>
2015-11-10 21:04:13 -0500

    9.2 Completed and tested.  Works good.
    Had to correct Random command to Rnd and fix the range settings to proper syntax.

---
+++ Lab 9.2/Module1.vb
@@ -0,0 +1,47 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'declaring variables
+        Dim pingCounter As Integer = 0
+        generateRandom(pingCounter)
+        Console.WriteLine("The server was pinged " & pingCounter & " times.")
+        Console.WriteLine("This concludes the test.  Press enter to exit.")
+        Console.ReadLine()
+
+    End Sub
+
+    Function generateRandom(ByRef pingCounter As Integer)
+
+        Dim randomNumber As Integer = 0
+        Dim loopCounter As Integer = 0
+        Dim loopsLeft As Integer = 0
+
+        Do While loopCounter < 200
+
+            loopsLeft = 200 - loopCounter
+            randomNumber = Int((Rnd() * 20) + 1)
+            Console.WriteLine("The random number is " & randomNumber & " There are " & loopsLeft & " attempts left.")
+            Console.WriteLine("")
+
+            If randomNumber = 5 Then
+                pingMe()
+                pingCounter = pingCounter + 1
+            End If
+
+            System.Threading.Thread.Sleep(1000)
+            loopCounter = loopCounter + 1
+        Loop
+
+        Return pingCounter
+
+    End Function
+
+    Sub pingMe()
+
+        Shell("Ping.exe 127.0.0.1", , True)
+        Console.WriteLine("")
+
+    End Sub
+
+End Module
---
+++ Lab 9.2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Lab 9.2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Lab 9.2")>
+<Assembly: AssemblyCopyright("Copyright ©  2015")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3b170d93-f41c-4d85-b795-ea30dc9e0270")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
